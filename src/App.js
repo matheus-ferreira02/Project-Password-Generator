f3ad461e731c484980f493460c87833376f92fe0
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-02-15 13:30:00 -0300

    first commmit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import './App.css';
+
+function App() {
+  return (
+    <section className="App">
+      <Header />
+      <Main />
+    </section>
+  );
+}
+
+export default App;
